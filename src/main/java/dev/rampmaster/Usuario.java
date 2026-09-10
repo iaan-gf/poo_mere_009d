ac01603c026fae9b18036e272d96e3513ea9fd25
iaan-gf <ia.grandon@duocuc.cl>
2026-09-10 16:37:47 -0600

    un pequeño abanse del repo don kiwi

---
+++ src/main/java/dev/rampmaster/Usuario.java
@@ -0,0 +1,19 @@
+package dev.rampmaster;
+
+public class Usuario {
+
+   private String nombre;
+   private String apellido;
+   private String correo;
+   private String run;
+
+   public Usuario(String nombre, String apellido, String correo, String run){
+       this.nombre = nombre;
+       this.apellido = apellido;
+       this.correo = correo;
+       this.run = run;
+
+       extends
+   }
+
+}
